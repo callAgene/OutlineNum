ef3f42d758b87fbc69b68d626d63656bce3fa58a
callAgene <57208890+callAgene@users.noreply.github.com>
2023-10-21 03:35:25 +0000

    Update Add_numbering.R

--- R/Add_numbering.R
+++ R/Add_numbering.R
@@ -14,7 +14,7 @@ Add_numbering <- function() {
   # Initialize variables to track header levels
   header_levels <- rep(0, 10)  # Assuming a maximum of 10 header levels
 
-if （lines==""）{
+if (lines==""){
 
   return("Need to select all first.")
 
